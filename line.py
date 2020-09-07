3ff766b67d733bc1766e65f353a95d9781d7c6cb
rba12 <rba12@duke.edu>
2020-09-07 11:00:57 -0400

    finished lineequation function that passes unit test

--- line.py
+++ line.py
@@ -1,2 +1,8 @@
-def findpoint((x1, y1), (x2, y2), a):
-	
+def findpoint(x1, x2, a):
+    slope = (x2[1]-x1[1])/(x2[0]-x1[0])
+    y = slope*(a -x1[0])+x1[0]
+    print(y)
+    return y
+
+
+findpoint((0,0),(1,1),6)
